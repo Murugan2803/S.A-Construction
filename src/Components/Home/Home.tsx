a078fae03f9599a1ca65634dda06120a4a0333ec
Murugan2803 <muruganm0306@gmail.com>
2026-01-30 01:16:21 +0530

    Updating UI Design in Home page

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -2,12 +2,6 @@ import React, { type JSX } from "react";
 import "../../CSS/Home.css";
 
 const Home: React.FC = (): JSX.Element => {
-  const services: string[] = [
-    "Residential Construction",
-    "Commercial Projects",
-    "Renovation",
-    "Interior Design",
-  ];
   return (
     <div className="home">
       {/* HERO SECTION */}
@@ -19,24 +13,52 @@ const Home: React.FC = (): JSX.Element => {
           </p>
         </div>
       </section>
-      {/* SERVICES SECTION */}
-      <section className="services py-5">
-        <div className="container">
-          <h2 className="text-center fw-bold mb-5">Our Services</h2>
-          <div className="row g-4">
-            {services.map((title: string, index: number) => (
-              <div key={index} className="col-lg-3 col-md-6 col-sm-12">
-                <div className="service-card h-100">
-                  <h5>{title}</h5>
-                  <p>
-                    High-quality solutions built with experience and trust.
-                  </p>
-                </div>
-              </div>
-            ))}
+      {/* ABOUT SUCCESS SECTION */}
+      <section className="success-wrapper">
+        <div className="success-card">
+
+          {/* LEFT */}
+          <div className="success-left">
+            <h6>Build Your Dream</h6>
+            <h2>
+              25 Years Of Undefeated <br /> Success
+            </h2>
+
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque in ipsum id orci porta dapibus. Vivamus magna
+              justo, lacinia eget consectetur sed, convallis.
+            </p>
+
+            <button>WORK WITH US</button>
           </div>
+
+          {/* RIGHT */}
+          <div className="success-right">
+            <div className="stat">
+              <h3>512+</h3>
+              <p>Successfully Project Finished.</p>
+            </div>
+
+            <div className="stat">
+              <h3>25+</h3>
+              <p>Years of experience with proud</p>
+            </div>
+
+            <div className="stat">
+              <h3>1120+</h3>
+              <p>Revenue in 2017 investment</p>
+            </div>
+
+            <div className="stat">
+              <h3>1520+</h3>
+              <p>Colleagues & counting more daily</p>
+            </div>
+          </div>
+
         </div>
       </section>
+
       {/* CTA SECTION */}
       <section className="cta text-center text-white py-5">
         <div className="container">
